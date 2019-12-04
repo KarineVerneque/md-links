c9a30611b4e6d12b05d411da8a9b9e9e6f028bac
Karine Verneque <you@example.com>
2019-12-03 21:33:07 -0300

    completed project

--- cli.js
+++ cli.js
@@ -6,6 +6,5 @@ const path = process.argv[2]
 mdLinks(path)
   .then((snap) => {
     snap.forEach(link => console.log(`Text: ${link.text} \n Href: ${link.href}`))
-    // console.log(valorPassadoPeloResolve)
   })
   .catch(console.error);
