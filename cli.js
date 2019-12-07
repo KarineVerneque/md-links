aee2fb20872150627492b1e6ce8822866904be07
Karine Verneque <you@example.com>
2019-12-07 13:53:33 -0300

    eslint configuration

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
-const mdLinks = require("./lib/index.js");
+const mdLinks = require('./lib/index.js');
 
-const path = process.argv[2]
+const path = process.argv[2];
 
 mdLinks(path)
-  .then((snap) => {
-    snap.forEach(link => console.log(`Text: ${link.text} \n Href: ${link.href}`))
-  })
-  .catch(console.error);
+    .then((snap) => {
+        snap.forEach(link => console.log(`Text: ${link.text} \n Href: ${link.href}`));
+    })
+    .catch(console.error);
